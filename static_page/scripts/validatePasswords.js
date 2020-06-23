d18180f05c4485f11a34eb7709bb7ef544292bba
sjagoori <sjagoori@users.noreply.github.com>
2020-06-23 12:54:58 +0200

    [Updated] Additional comments

--- static_page/scripts/validatePasswords.js
+++ static_page/scripts/validatePasswords.js
@@ -9,15 +9,18 @@ function validatePasswords() {
   const y = document.forms['registerForm']['rpassword'];
   const z = document.getElementById('errorMessage');
 
+  // the error message is created
   const errorMessage = document.createElement('span');
   errorMessage.setAttribute('id', 'errorMessage');
   errorMessage.textContent = 'Password does not match.';
   errorMessage.style.color = '#FF0000';
 
   if (x.value != y.value) {
+    // if the error message does not already exist, it will be added after the repeat-password field in the DOM.
     if (z == null) {
       y.after(errorMessage);
     }
+    // passwords do not match
     return false;
   }
 }
